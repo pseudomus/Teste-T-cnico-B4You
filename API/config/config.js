2d68bb0eb19d669a11206b5f08e715e38f524895
Luca Lacerda <lucagabriel520@gmail.com>
2025-07-22 18:23:59 -0300

    Docker added and seeder created

--- API/config/config.js
+++ API/config/config.js
@@ -3,23 +3,23 @@ require('dotenv').config();
 module.exports = {
   development: {
     username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || null,
-    database: process.env.DB_NAME || 'database_development',
-    host: process.env.DB_HOST || '127.0.0.1',
+    password: process.env.DB_PASSWORD || 'root',
+    database: process.env.DB_NAME || 'products_db',
+    host: process.env.DB_HOST || 'localhost',
     dialect: 'mysql',
   },
   test: {
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || null,
-    database: process.env.DB_NAME ? process.env.DB_NAME + '_test' : 'database_test',
-    host: process.env.DB_HOST || '127.0.0.1',
+    username: 'root',
+    password: 'root',
+    database: 'products_db_test',
+    host: 'localhost',
     dialect: 'mysql',
   },
   production: {
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || null,
-    database: process.env.DB_NAME ? process.env.DB_NAME + '_prod' : 'database_production',
-    host: process.env.DB_HOST || '127.0.0.1',
+    username: 'root',
+    password: 'root',
+    database: 'products_db_prod',
+    host: 'localhost',
     dialect: 'mysql',
   }
 };
---
+++ API/seeders/20250722211533-demo-products.js
@@ -0,0 +1,54 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('products', [
+      {
+        name: 'iPhone 14',
+        price: 4999.99,
+        category: 'Eletrônicos',
+        description: 'Último modelo da Apple com câmera avançada e tela OLED.',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Tênis de Corrida Adidas',
+        price: 299.90,
+        category: 'Esportes e Lazer',
+        description: 'Leve e confortável, ideal para atividades físicas.',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'O Poder do Hábito',
+        price: 39.90,
+        category: 'Livros e Papelaria',
+        description: 'Livro best-seller sobre formação de hábitos.',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Fone de Ouvido Bluetooth',
+        price: 199.99,
+        category: 'Eletrônicos',
+        description: 'Som de alta qualidade e cancelamento de ruído.',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Bolsa Feminina de Couro',
+        price: 450.00,
+        category: 'Beleza e Cuidados Pessoaisv',
+        description: 'Elegante e espaçosa, perfeita para o dia a dia.',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('products', null, {});
+
+  }
+};
